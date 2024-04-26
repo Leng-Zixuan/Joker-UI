408bb42073ce558673c69b596640899d19bed388
jinyuxuan <jyx13164683568@163.com>
2024-04-26 16:13:51 +0800

    .

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "prettier.useTabs": false, // 不使用tab
+    singleQuote: false, // 是否必须要用单引号
+    semi: true, // 结尾是否加分号
+    bracketSpacing: true, // 括号空格
+    htmlWhitespaceSensitivity: "ignore", // 忽略'>'下落问题
+    "javascript.format.insertSpaceBeforeFunctionParenthesis": true,
+    "files.insertFinalNewLine": true,
+    printWidth: 160,
+    arrowParens: "always",
+    jsxBracketSameLine: true,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath } from "node:url";
 import { resolve } from "node:path";
 
-const __dirname = fileURLToPath(new URL("../", import.meta.url));
+const __dirname = fileURLToPath(new URL("./", import.meta.url));
 
 export default defineConfig((config) => {
   const { command, mode } = config;
